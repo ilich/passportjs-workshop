1e58358759b71ba651491bdda6e49394805bd9c6
Ilya Verbitskiy <iverbitskiy@gmail.com>
2015-10-20 13:42:28 -0500

    Create account

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var bcrypt = require('bcrypt-nodejs')
+var db = require('../db');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
@@ -14,7 +16,8 @@ router.post('/', function(req, res, next) {
     res.render('index', { 
         scripts: [], 
         errors: errors 
-    });
+    });  
+    
 });
 
 router.get('/register', function(req, res, next) {
@@ -27,10 +30,45 @@ router.get('/register', function(req, res, next) {
 router.post('/register', function(req, res, next) {
     var errors = [];
     
-    res.render('register', { 
-        scripts: [ '/js/register.js' ], 
-        errors: errors 
-    });
+    if (!/^[A-Za-z0-9]+$/g.test(req.body.username)) {
+        errors.push('Invalid username.');
+    }
+    
+    if (req.body.password.length === 0) {
+        errors.push('Password is required.');
+    }
+    
+    if (req.body.password !== req.body.confirmPassword) {
+        errors.push('Passwords do not match.')    
+    }
+    
+    if (errors.length > 0) {
+        return res.render('register', { 
+            scripts: [ '/js/register.js' ], 
+            errors: errors 
+        });   
+    }
+    
+    var users = db.get().collection('users');
+    users.findOne({ username: req.body.username }, function (err, user) {
+        if (user !== null) {
+            errors.push('Invalid username.');
+            
+            return res.render('register', { 
+                scripts: [ '/js/register.js' ], 
+                errors: errors 
+            });
+        }
+        
+        bcrypt.hash(req.body.password, null, null, function (err, hash) {
+            users.insert({
+                username: req.body.username,
+                password: hash
+            });
+            
+            res.redirect('/profile');  
+        });
+    });    
 });
 
 router.get('/profile', function (req, res, next) {
