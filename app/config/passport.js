9ba35f306e5b76a3efe867b3d080b8e4598ac23e
Ilya Verbitskiy <iverbitskiy@gmail.com>
2015-10-20 15:31:16 -0500

    Use Passport Local Strategy to create accounts

--- app/config/passport.js
+++ app/config/passport.js
@@ -37,13 +37,77 @@ module.exports = function (passport) {
                         if (err) {
                             return done(err);
                         } else if (result) {
-                            return done(null, user);
+                            user.counter++;
+                            users.update({ _id: new ObjectID(user._id) }, { $set: { counter: user.counter } }, function (err) {
+                                if (err) {
+                                    return done(err);
+                                }    
+                                
+                                return done(null, user);    
+                            });
                         } else {
                             return done(null, false, { message: 'Incorrect username or password.' });
                         }
                     });
                 }
             });  
+        }
+    ));
+    
+    passport.use('local-signup', new LocalStrategy({
+            usernameField: 'username',
+            passwordField: 'password',
+            passReqToCallback: true
+        },
+        function (req, username, password, done) {
+            var errors = [];
+    
+            if (!/^[A-Za-z0-9]+$/g.test(req.body.username)) {
+                errors.push('Invalid username.');
+            }
+            
+            if (req.body.password.length === 0) {
+                errors.push('Password is required.');
+            }
+            
+            if (req.body.password !== req.body.confirmPassword) {
+                errors.push('Passwords do not match.')    
+            }
+            
+            if (errors.length > 0) {
+                return done(null, false, { message: errors.join('|') });  
+            }
+            
+            var users = db.get().collection('users');
+            users.findOne({ username: req.body.username }, function (err, user) {
+                if (err) {
+                    return done(err);    
+                }
+                
+                if (user !== null) {
+                    return done(null, false, { message: 'Invalid username.' });
+                }
+                
+                bcrypt.hash(req.body.password, null, null, function (err, hash) {
+                    if (err) {
+                        return done(err);    
+                    }
+                    
+                    var user = {
+                        username: req.body.username,
+                        password: hash,
+                        counter: 1
+                    }
+                    
+                    users.insert(user, function (err) {
+                        if (err) {
+                            return done(err);
+                        }
+                        
+                        return done(null, user);
+                    });
+                });
+            }); 
         }
     ));
 };
--- app/routes/index.js
+++ app/routes/index.js
@@ -5,13 +5,17 @@ var db = require('../db');
 var secure = require('../middleware/secure');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    var errors = [];
-    var fail = req.flash('error'); 
-    
-    if (fail.length > 0) {
-        errors.push(fail);
+function getErrors(req) {
+    var fail = req.flash('error');
+    if (fail.length === 0) {
+        return []
     }
+    
+    return fail[0].split('|');
+}
+
+router.get('/', function(req, res, next) {
+    var errors = getErrors(req);
     
     res.render('index', { 
         scripts: [], 
@@ -27,55 +31,19 @@ router.post('/', passport.authenticate('local-signin', {
 }));
 
 router.get('/register', function(req, res, next) {
+    var errors = getErrors(req);
+    
     res.render('register', { 
         scripts: [ '/js/register.js' ], 
-        errors: [] 
+        errors: errors
     });
 });
 
-router.post('/register', function(req, res, next) {
-    var errors = [];
-    
-    if (!/^[A-Za-z0-9]+$/g.test(req.body.username)) {
-        errors.push('Invalid username.');
-    }
-    
-    if (req.body.password.length === 0) {
-        errors.push('Password is required.');
-    }
-    
-    if (req.body.password !== req.body.confirmPassword) {
-        errors.push('Passwords do not match.')    
-    }
-    
-    if (errors.length > 0) {
-        return res.render('register', { 
-            scripts: [ '/js/register.js' ], 
-            errors: errors 
-        });   
-    }
-    
-    var users = db.get().collection('users');
-    users.findOne({ username: req.body.username }, function (err, user) {
-        if (user !== null) {
-            errors.push('Invalid username.');
-            
-            return res.render('register', { 
-                scripts: [ '/js/register.js' ], 
-                errors: errors 
-            });
-        }
-        
-        bcrypt.hash(req.body.password, null, null, function (err, hash) {
-            users.insert({
-                username: req.body.username,
-                password: hash
-            });
-            
-            res.redirect('/profile');  
-        });
-    });    
-});
+router.post('/register', passport.authenticate('local-signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/register',
+    failureFlash: true
+}));
 
 router.get('/profile', secure, function (req, res, next) {
     res.render('profile', { 
