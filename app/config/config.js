20eb9933a5c26d6016f95970ba5d6d76f49b7c68
Ilya Verbitskiy <iverbitskiy@gmail.com>
2015-10-21 08:59:47 -0500

    Added Twitter configuration

--- app/config/config.js
+++ app/config/config.js
@@ -1,3 +1,8 @@
 module.exports = {
-    db: 'mongodb://localhost/passportjs-workshop'
+    db: 'mongodb://localhost/passportjs-workshop',
+    twitter: {
+        clientID: "Your Consumer Key (API Key)",
+        clientSecret: "Consumer Secret (API Secret)",
+        callbackURL: "http://localhost:3000/auth/twitter/callback"
+    }
 };
